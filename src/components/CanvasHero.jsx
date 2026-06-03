ec8dd71fbe5d86ab23055bc34215f59a241adbf5
Owais Raza <oraza7867@outlook.com>
2026-06-03 16:25:04 +0530

    canvas edit

--- src/components/CanvasHero.jsx
+++ src/components/CanvasHero.jsx
@@ -385,7 +385,7 @@ const CanvasHero = () => {
       <canvas ref={canvasRef} style={styles.canvas} />
       
       <motion.section style={{ ...styles.heroSection, opacity: opacityHero }}>
-        <img src={resolveAsset("/images/homeimg.png")} alt="Hero" style={styles.heroImage} />
+        <img src={resolveAsset("/sa/tedxiul/images/tedintegral.png")} alt="Hero" style={styles.heroImage} />
       </motion.section>
 
       <div style={styles.scrollSpace} />
